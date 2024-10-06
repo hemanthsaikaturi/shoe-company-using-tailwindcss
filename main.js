09b9011aa7d41aaa8bd67e7ee507507e270b6080
hemanth <hemanthsaikaturi@gmail.com>
2024-10-06 14:40:56 +0530

    moved files from src

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const hamburgerMenu = document.querySelector('#hamburger-menu')
+const header = document.querySelectorAll('header')
 
 hamburgerMneu.addEventListener('click', () => {
     hamburgerMneu.classList.add('active')
